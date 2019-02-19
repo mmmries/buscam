7d9ac0b43a604b595634d7ddee81e609584e2077
Michael Ries <michael@riesd.com>
2019-02-19 14:55:07 -0700

    nerves.new --init-gadget buscam

---
+++ lib/buscam.ex
@@ -0,0 +1,18 @@
+defmodule Buscam do
+  @moduledoc """
+  Documentation for Buscam.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Buscam.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/buscam_test.exs
@@ -0,0 +1,8 @@
+defmodule BuscamTest do
+  use ExUnit.Case
+  doctest Buscam
+
+  test "greets the world" do
+    assert Buscam.hello() == :world
+  end
+end
